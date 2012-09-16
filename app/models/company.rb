2d3a34a8437fd3f4bed7553b8bb39190c99bfa16
unknown <Administrator@celab2-19.comp1-domain.spit.ac.in>
2012-09-16 15:35:24 +0530

    done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,10 @@
 class Company < ActiveRecord::Base
+  validate symbol.length_should_be_three_characters
+  def    symbol.length_should_be_three_characters
+    if symbol.length>3
+      errors.add(:symbol,'invalid')
+
+    end
+    end
+  end
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,10 @@
 class Investment < ActiveRecord::Base
+  validate :qty_should_be_positive
+  def qty_should_be_positive
+    if qty.to_i < 0
+      errors.add(:qty,'should be positive')
+
+    end
+  end
+
 end
